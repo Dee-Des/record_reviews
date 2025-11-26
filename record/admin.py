5fb4b5cc6a252aca2a20ce740bf373d2f169216d
Dee-Des <desmonddee2014@gmail.com>
2025-11-26 10:41:29 +0000

    use record model to update the database

--- record/admin.py
+++ record/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Record
 
 # Register your models here.
+# import and register the Record 
+# model to allow to create, update 
+# and delete records from the admin panel 
+admin.site.register(Record)
