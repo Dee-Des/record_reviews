2cf753db7a33a9c197fac3a90213ac744d15b7d8
Dee-Des <desmonddee2014@gmail.com>
2025-11-27 11:47:54 +0000

    add code to record admin.py file to enable faser content searches and modify the right-hand filter panel

--- record/admin.py
+++ record/admin.py
@@ -7,8 +7,8 @@ from django_summernote.admin import SummernoteModelAdmin
 class RecordAdmin(SummernoteModelAdmin):
 
     list_display = ('title', 'status', 'created_on')
-    search_fields = ['title']
-    list_filter = ('status',)
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on',)
     summernote_fields = ('content',)
 
 
