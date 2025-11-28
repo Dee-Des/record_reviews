e43a7f8da183225e52a6ccf090f48a7a565d191b
Dee-Des <desmonddee2014@gmail.com>
2025-11-28 13:14:52 +0000

    add record_detail view

--- record/urls.py
+++ record/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.RecordList.as_view(), name='home'),
+    # path('<slug:slug>/', views.record_detail, name='record_detail'),
+    path('<created_on:created_on>/', views.record_detail, name='record_detail'),
 ]
