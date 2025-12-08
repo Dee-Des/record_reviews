f43833736943c702846257e23350a70c52b3f7f5
Dee-Des <desmonddee2014@gmail.com>
2025-12-08 10:40:35 +0000

    revert in reviews.js file

--- record/admin.py
+++ record/admin.py
@@ -1,20 +1,21 @@
 from django.contrib import admin
-from .models import Record, Review # these models 
+from .models import Record, Review
 from django_summernote.admin import SummernoteModelAdmin
 from .forms import RecordForm
 
-@admin.register(Record) # decorator - how register a class - which allows customise how registered models
-# will appear on the admin site
+
+# decorator - how register a class - which allows
+# customise how registered models will appear on the admin site
+@admin.register(Record)
 class RecordAdmin(SummernoteModelAdmin):
 
-    form = RecordForm 
+    form = RecordForm
     list_display = ('title', 'status', 'created_on')
     search_fields = ['title', 'content']
     list_filter = ('status', 'created_on',)
     summernote_fields = ('content',)
 
- 
+
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
-    pass   
-   
+    pass
